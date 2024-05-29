e869830966fe2538b40b09a32358e33762c014f6
Enzo Zarate <enzozarateok66@gmail.com>
2024-05-29 20:24:01 -0300

    reescripcion test

--- unidad 3/parcial2_8-6-2023/claseTest.py
+++ unidad 3/parcial2_8-6-2023/claseTest.py
@@ -3,15 +3,19 @@ from Casa import Casa
 from Departamento import Departamento
 
 class TestInmuebles(unittest.TestCase):
-    def test_importeVenta(self):
-        depto = Departamento(localidad="Loc", direccion="Dir", supC=100, cantH=1, nroM=2, nroDm=3, piso=4)
-        casa = Casa(localidad="Loc", direccion="Dir", supC=200, supT=300)
+    def setUp(self):
+        self.__depto = Departamento(localidad="Loc", direccion="Dir", supC=100, cantH=1, nroM=2, nroDm=3, piso=4)
+        self.__casa = Casa(localidad="Loc", direccion="Dir", supC=200, supT=300)
+    def test_precioConstDepto(self):
+        self.assertEqual(self.__depto.getPrecioConstruccion(),1*17000)
+    def test_precioConstCasa(self):
+        self.assertEqual(self.__casa.getPrecioConstruccion(),300*1.8*20000)
+    def test_importeVentaDepto(self):
+        impEsperadoDepto = 100 * 15 * self.__depto.getPrecioConstruccion()
+        self.assertEqual(self.__depto.getImporteVenta(), impEsperadoDepto)
+    def test_importeVentaCasa(self):
+        impEsperadoCasa = 200 * 15 * self.__casa.getPrecioConstruccion() 
+        self.assertEqual(self.__casa.getImporteVenta(), impEsperadoCasa)
 
-        precioConstDepto = 1*17000
-        precioConstCasa = 300*1.8*20000
-
-        impEsperadoDepto = 100 * 15 * precioConstDepto
-        impEsperadoCasa = 200 * 15 * precioConstCasa 
-        
-        self.assertEqual(depto.getImporteVenta(), impEsperadoDepto)
-        self.assertEqual(casa.getImporteVenta(), impEsperadoCasa)
+if __name__=='__main__':
+    unittest.main()
